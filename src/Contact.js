386e71017c21a9589c194518a6e981ec4099b9ee
Deetchana-26 <Deetchana-26>
2023-11-01 15:12:29 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,30 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="deetchana.d2021@vitstudent.ac.in">Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/deetchana-dharani-64317a270/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/Deetchana-26 " target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Deetchana D</h1>
+      <p>Full Stack Web Developerr</p>
+    </header>
+  );
+}
+
+export default Header;
